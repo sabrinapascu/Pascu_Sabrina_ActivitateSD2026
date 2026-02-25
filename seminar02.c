fd6e4b2088873783f2977880414d0dd5b4486c6c
sabrinapascu <pascumaria24@stud.ase.ro>
2026-02-25 18:01:06 +0200

    adaugat seminar02

---
+++ seminar02.c
@@ -0,0 +1,130 @@
+#include<stdio.h>
+//#include<malloc.h>
+#include<stdlib.h>
+#include<string.h>
+
+
+struct Film {
+	int id;
+	int durata;
+	char* nume;
+	float buget;
+	char varstaMinima;
+};
+struct Film initializare(int _id, int _durata, const char* _nume, float _buget, char _varstaMinima) {
+	struct Film s;
+	s.id = _id;
+	s.durata=_durata;
+	s.nume=malloc((strlen(_nume)+1)*sizeof(char));
+	strcpy(s.nume, _nume);
+	s.buget=_buget;
+	s.varstaMinima=_varstaMinima;
+
+	return s;
+}
+
+struct Film copyFilm(struct Film s) {
+	return initializare(s.id, s.durata, s.nume, s.buget, s.varstaMinima);
+}
+
+void afisare(struct Film s) {
+	printf("id: %d\n", s.id);
+	printf("nume: %s\n", s.nume);
+	printf("durata: %d\n", s.durata);
+	printf("buget: %f\n", s.buget);
+	printf("varstaMinima: %c\n", s.varstaMinima);
+
+}
+
+void afisareVector(struct Film* vector, int nrElemente) {
+	for (int i=0; i<nrElemente; i++) {
+		afisare(vector[i]);
+	}
+}
+
+struct Film* copiazaPrimeleNElemente(struct Film* vector, int nrElemente, int nrElementeCopiate) {
+	struct Film *vectorNou=malloc(nrElementeCopiate*sizeof(struct Film));
+	for (int i=0; i<nrElementeCopiate; ++i)
+	{
+		vectorNou[i]=copyFilm(vector[i]);
+	}
+
+	return vectorNou;
+}
+
+void dezalocare(struct Film** vector, int* nrElemente) {
+	for (int i=0; i<*nrElemente; ++i) {
+		free(((*vector)[i]).nume);
+	}
+	free(*vector);
+	*vector=NULL;
+	*nrElemente=0;
+}
+
+void copiazaAnumiteElemente(struct Film* vector, char nrElemente, float bugetMaxim, struct Film** vectorNou, int* dimensiune) {
+	//parametrul prag poate fi modificat in functie de
+	// tipul atributului ales pentru a indeplini o conditie
+	//este creat un nou vector cu elementele care indeplinesc acea conditie
+	*dimensiune=0;
+	for (int i=0; i<nrElemente; i++) {
+		if (vector[i].buget<bugetMaxim) {
+			(*dimensiune)++;
+		}
+	}
+	(*vectorNou)=malloc((*dimensiune)*sizeof(struct Film));
+	int k=0;
+	for (int i=0; i<nrElemente; i++) {
+		if (vector[i].buget<bugetMaxim) {
+			(*vectorNou)[k]=copyFilm(vector[i]);
+			k++;
+		}
+	}
+}
+
+struct Film getPrimulElementConditionat(struct Film* vector, int nrElemente, const char* numeCautat) {
+	//trebuie cautat elementul care indeplineste o conditie
+	//dupa atributul de tip char*. Acesta este returnat.
+	struct Film s;
+	s.id = -1;
+	s.nume=NULL;
+	for (int i=0; i<nrElemente; i++) {
+		if (!strcmp(vector[i].nume, numeCautat)) {
+			return s;
+		}
+	}
+	return s;
+}
+
+
+int main() {
+
+	struct Film f=initializare(1, 120, "Dune", 12, 16);
+	afisare(f);
+	int nrFilme=3;
+	struct Film* filme=malloc(nrFilme* sizeof(struct  Film));
+	filme[0]=f;
+	filme[1]=initializare(2, 240, "James Bond", 16, 20);
+	filme[2]=initializare(3, 240, "Star Wars", 25, 12);
+
+	afisareVector(filme, nrFilme);
+
+	int nrFilmeCopiate=2;
+	struct Film* vectorNou;
+	vectorNou=copiazaPrimeleNElemente(filme, nrFilme, nrFilmeCopiate);
+	printf("elemente copiate: \n");
+	afisareVector(vectorNou, nrFilmeCopiate);
+	printf("\n");
+
+	dezalocare(&vectorNou, &nrFilmeCopiate);
+
+	struct Film* filmeIeftine;
+	float prag=18;
+	int dimensiune;
+	copiazaAnumiteElemente(filme, nrFilme, prag, &filmeIeftine, &dimensiune);
+	afisareVector(filmeIeftine, dimensiune);
+	afisare(getPrimulElementConditionat(filme, nrFilme, "Dune"));
+	dezalocare(&filme, &nrFilme);
+
+
+	return 0;
+}
